d374e7dc4da7c07fa974bdd27eb5a01c1be53c25
nillmishra <nillmishra09@gmail.com>
2025-08-08 09:59:44 +0530

    saved

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
 export const LOGO_URL = "https://cdn.dribbble.com/userupload/9903003/file/original-8eae5aec60527b67b7678e42aa2d8645.jpg?resize=752x&vertical=center";
-=======
-export const LOGO_URL = "https://template.canva.com/EAE8NIYvWC8/1/0/1600w-5Y6NC30XBvo.jpg";
->>>>>>> d09e5e44c2978c257ab3a20c09e7475024d300dc
 export const CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 export const MENU_URL ="https://www.swiggy.com/mapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=23.6888636&lng=86.9660638&restaurantId="
