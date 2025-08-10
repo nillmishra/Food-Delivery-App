33300f11f5003c3b2aa842d6792f8f6306e8d707
nillmishra <nillmishra09@gmail.com>
2025-08-10 11:24:47 +0530

    updated my code for dev

--- src/component/Contact.js
+++ src/component/Contact.js
@@ -1,9 +1,25 @@
+import React, { useContext } from "react";
+import UserContext from "../utils/UserContext";
+
 const Contact = () => {
-    return(
-        <div>
-            Hello Contact!
-        </div>
-    )
-}
+  const {loggedInUser, setUserName} = useContext(UserContext); 
+
+  return (
+    <div>
+      <div>
+        loggedInUser: <span>{loggedInUser}</span>
+      </div>
+      <div className="search-bar">
+        <input
+          type="text"
+          placeholder="write username..."
+          aria-label="Search"
+          value={loggedInUser}
+          onChange={(e) => setUserName(e.target.value)}
+        />
+      </div>
+    </div>
+  );
+};
 
 export default Contact;
