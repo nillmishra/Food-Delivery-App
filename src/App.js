1f7514de53fc24bec47805880725b3ac540be5ec
nillmishra <nillmishra09@gmail.com>
2025-08-11 11:19:20 +0530

    testing added

--- src/App.js
+++ src/App.js
@@ -5,16 +5,36 @@ import Body from "./component/Body";
 import Error from "./component/Error";
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import ResturantMenu from "./component/ResturantMenu";
+import UserContext from "./utils/UserContext";
+import { Provider } from "react-redux";
+import appStore from "./utils/appStore";
+import Cart from "./component/Cart"
 
+const Grocery = lazy(() => import("./component/Grocery"));
+const About = lazy(() => import("./component/About"));
+const Contact = lazy(() => import("./component/Contact"));
 
-const Grocery = lazy(()=> import("./component/Grocery"))
+const AppLayout = () => {
+  const [userName, setUserName] = useState();
 
-const AppLayout = () => (
-  <div className="App">
-    <Header />
-    <Outlet />
-  </div>
-);
+  useEffect(() => {
+    const data = {
+      name: "Nill Mishra",
+    };
+    setUserName(data.name);
+  }, []);
+
+  return (
+    <Provider store={appStore}>
+      <UserContext.Provider value={{ loggedInUser: userName, setUserName }}>
+        <div className="App">
+          <Header />
+          <Outlet />
+        </div>
+      </UserContext.Provider>
+    </Provider>
+  );
+};
 
 const appRouter = createBrowserRouter([
   {
@@ -22,27 +42,44 @@ const appRouter = createBrowserRouter([
     element: <AppLayout />,
     children: [
       {
-    path: "/",
-    element: <Body />,
-  },
-  {
-    path: "/about",
-    element: <About />,
-  },
-  {
-    path: "/contact",
-    element: <Contact />,
-  },
-  {
-    path: "/grocery",
-    element: <Suspense fallback={<h1>Loading...</h1>}><Grocery/></Suspense>,
-  },
-  {
-    path: "/resturants/:resId",
-    element: <ResturantMenu/>,
-  },
-],
-errorElement: <Error/>
+        path: "/",
+        element: <Body />,
+      },
+      {
+        path: "/about",
+        element: (
+          <Suspense fallback={<h1>Loading....</h1>}>
+            <About />
+          </Suspense>
+        ),
+      },
+      {
+        path: "/contact",
+        element: (
+          <Suspense fallback={<h1>Loading....</h1>}>
+            <Contact />
+          </Suspense>
+        ),
+      },
+      {
+        path: "/grocery",
+        element: (
+          <Suspense fallback={<h1>Loading...</h1>}>
+            <Grocery />
+          </Suspense>
+        ),
+      },
+      {
+        path: "/resturants/:resId", // Consider spelling: /restaurants/:resId
+        element: <ResturantMenu />,
+      },
+      {
+        path: "/cart",
+        element: <Cart />,
+
+      },
+    ],
+    errorElement: <Error />,
   },
 ]);
 
